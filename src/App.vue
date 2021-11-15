a31ccb3f27cc976e3adc8c41d9905e0b799ec83e
Oleg Makeev <m.oleg346@gmail.com>
2021-11-15 23:48:56 +0600

    Header

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,42 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> | 
-    <router-link to="/description">Details</router-link>
+  <div class="main-container">
+    <div class="sidebar">
+
+    </div>
+    <div>
+        <Header/>
+        <div id="nav">
+          <router-link to="/">Home</router-link> | 
+          <router-link to="/description">Details</router-link>
+        </div>
+        <router-view/>
+    </div>
   </div>
-  <router-view/>
 </template>
+
+<script>
+import Header from "@/componets/Header";
+export default {
+	components: { Header },
+};
+</script>
 
 <style lang="scss">
 @import '@/assets/scss/index.scss';
 body{
     background-color: $bg-main;
+}
+
+body, h3, ul, div, span {
+    padding: 0;
+    margin: 0;
+}
+
+.main-container {
+  display: flex;
+  .sidebar {
+    width: vw(330);
+  }
 }
 
 #nav{
