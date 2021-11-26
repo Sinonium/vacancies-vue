f56f45cf2443f61772518d6eb1cc3d10ad337eaf
Sinonium <sinoniumx@gmail.com>
2021-11-27 04:42:32 +0600

    huge sidebar refactor and scrollbar

--- src/components/sidebar/categories/business/SubCategories.vue
+++ src/components/sidebar/categories/business/SubCategories.vue
@@ -1,5 +1,5 @@
 <template>
-   <ul>
+   <ul class="sub">
       <li
          :class="{ clicked: clickedCategories === 1 }"
          @click="clickedCategories = 1"
@@ -57,38 +57,3 @@
       },
    };
 </script>
-
-<style lang="scss" scoped>
-   @import "@/assets/scss/index.scss";
-   ul {
-      margin-left: vw(70);
-      li {
-         list-style: none;
-         margin-bottom: vw(15);
-         cursor: pointer;
-         span {
-            position: relative;
-            @include font(vw(12), 700, vw(20), $greyBlue80);
-         }
-         span::before {
-            content: "";
-            position: absolute;
-            left: vw(-31);
-            bottom: 35%;
-            background-color: #fff;
-            width: vw(6.6);
-            height: vw(6.6);
-            border-radius: 50%;
-         }
-      }
-      li.clicked {
-         span {
-            color: #ffffff;
-         }
-         span::before {
-            background-color: $green;
-
-         }
-      }
-   }
-</style>
