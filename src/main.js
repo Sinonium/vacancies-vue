f4bb778aa33e8e3aa8b02023efaf07bcb0f3d072
Oleg Makeev <m.oleg346@gmail.com>
2021-12-08 17:34:35 +0600

    App change

--- src/main.js
+++ src/main.js
@@ -2,15 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { onAuthStateChanged } from "firebase/auth"
-import {auth} from './firebase/config'
 
-let app = null;
-
-onAuthStateChanged(auth, () => {
-    if (!app) {
-        app = createApp(App).use(store).use(router).mount('#app');
-    }
-})
+createApp(App).use(store).use(router).mount('#app');
 
 
