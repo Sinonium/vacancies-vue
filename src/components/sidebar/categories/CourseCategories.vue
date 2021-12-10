5dab74c39a154f8eec63cb5468b609e9f2c9891f
Sinonium <sinoniumx@gmail.com>
2021-12-11 00:19:43 +0600

    vuex dispatch, commit fixes, home reactivity chang

--- src/components/sidebar/categories/CourseCategories.vue
+++ src/components/sidebar/categories/CourseCategories.vue
@@ -160,115 +160,124 @@
 </template>
 
 <script>
-import { computed, ref } from "@vue/reactivity";
-import { useStore } from "vuex";
-import SubDevCategories from "./development/SubCategories.vue";
-import SubBusCategories from "./business/SubCategories.vue";
-import SubFinanceCategories from "./finance/SubCategories.vue";
-import SubItSoftCategories from "./it&soft/SubCategories.vue";
-import SubOfficeCategories from "./office/SubCategories.vue";
-import SubPersonalCategories from "./personal/SubCategories.vue";
-import SubDesignCategories from "./design/SubCategories.vue";
-import SubMarketingCategories from "./marketing/SubCategories.vue";
-import SubLifeStyleCategories from "./lifestyle/SubCategories.vue";
-import SubPhotoCategories from "./photography/SubCategories.vue";
-import SubHealthCategories from "./health/SubCategories.vue";
-import SubMusicCategories from "./music/SubCategories.vue";
-import SubTeachCategories from "./teaching/SubCategories.vue";
-import { onMounted } from '@vue/runtime-core';
-export default {
-  components: {
-    SubDevCategories,
-    SubBusCategories,
-    SubFinanceCategories,
-    SubItSoftCategories,
-    SubOfficeCategories,
-    SubPersonalCategories,
-    SubDesignCategories,
-    SubMarketingCategories,
-    SubLifeStyleCategories,
-    SubPhotoCategories,
-    SubHealthCategories,
-    SubMusicCategories,
-    SubTeachCategories,
-  },
-  setup(props, context) {
-    const store = useStore();
+   import { computed, ref } from "@vue/reactivity";
+   import { useStore } from "vuex";
+   import SubDevCategories from "./development/SubCategories.vue";
+   import SubBusCategories from "./business/SubCategories.vue";
+   import SubFinanceCategories from "./finance/SubCategories.vue";
+   import SubItSoftCategories from "./it&soft/SubCategories.vue";
+   import SubOfficeCategories from "./office/SubCategories.vue";
+   import SubPersonalCategories from "./personal/SubCategories.vue";
+   import SubDesignCategories from "./design/SubCategories.vue";
+   import SubMarketingCategories from "./marketing/SubCategories.vue";
+   import SubLifeStyleCategories from "./lifestyle/SubCategories.vue";
+   import SubPhotoCategories from "./photography/SubCategories.vue";
+   import SubHealthCategories from "./health/SubCategories.vue";
+   import SubMusicCategories from "./music/SubCategories.vue";
+   import SubTeachCategories from "./teaching/SubCategories.vue";
+   export default {
+      components: {
+         SubDevCategories,
+         SubBusCategories,
+         SubFinanceCategories,
+         SubItSoftCategories,
+         SubOfficeCategories,
+         SubPersonalCategories,
+         SubDesignCategories,
+         SubMarketingCategories,
+         SubLifeStyleCategories,
+         SubPhotoCategories,
+         SubHealthCategories,
+         SubMusicCategories,
+         SubTeachCategories,
+      },
+      setup(props, context) {
+         const store = useStore();
 
-    const cata = async (cata, subcata) => {
-      category.value = cata;
-      subCategory.value = subcata;
-      // if(cata == category.value) {
-      //    subCategory.value = undefined
-      // }
-      if (subCategory.value) {
-        store.commit("SET_CATEGORY", {
-          main: mainCategory.value,
-          category: category.value,
-          sub: subCategory.value,
-        });
-      //   await store.dispatch("getAnyCollection");
-      } else {
-        store.commit("SET_CATEGORY", {
-          main: mainCategory.value,
-          category: category.value,
-        });
-        await store.dispatch("getAnyCollection", [
-          mainCategory.value,
-          category.value,
-        ]);
-      }
-    };
-    const mainCategory = ref("");
-    const category = ref("");
-    const subCategory = ref("");
-    const categoryActive = computed(() => store.state.categories.category);
-    const test = async (i) => {
-      mainCategory.value = i;
-      context.emit("clickedCata", i, category.value, subCategory.value);
-      store.commit("SET_CATEGORY", {
-        main: mainCategory.value,
-      });
-      await store.dispatch("getAnyCollection", [mainCategory.value]);
-    };
+         const mainCategory = ref("");
+         const category = ref("");
+         const subCategory = ref("");
 
-    const clickedCategories = ref(null);
-    const handleClick = (i) => {
-      if (clickedCategories.value !== i) {
-        clickedCategories.value = i;
-      } else {
-        clickedCategories.value = null;
-      }
-    };
+         const cata = async (cata, subcata) => {
+            subCategory.value = subcata;
+            if (cata !== category.value) {
+               store.commit("SET_CATEGORY", {
+                  main: mainCategory.value,
+                  category: category.value,
+                  sub: undefined,
+               });
+               category.value = cata;
+               subCategory.value = "";
+            }
+            category.value = cata;
 
-    onMounted(() => {
-       store.dispatch("getAnyCollection");
-    })
+            if (subCategory.value !== "") {
+               store.commit("SET_CATEGORY", {
+                  main: mainCategory.value,
+                  category: category.value,
+                  sub: subCategory.value,
+               });
+               store.dispatch("getAnyCollection", [
+                  mainCategory.value,
+                  category.value,
+                  subCategory.value,
+               ]);
+            } else {
+               store.commit("SET_CATEGORY", {
+                  main: mainCategory.value,
+                  category: category.value,
+               });
+               await store.dispatch("getAnyCollection", [
+                  mainCategory.value,
+                  category.value,
+               ]);
+            }
+         };
 
-    return {
-      mainCategory,
-      categoryActive,
-      category,
-      subCategory,
-      test,
-      cata,
-      handleClick,
-      clickedCategories,
-      arrowExpand: require("@/assets/img/sidebar/arrow.svg"),
-      devIcon: require("@/assets/img/sidebar/devIcon.svg"),
-      busIcon: require("@/assets/img/sidebar/busIcon.svg"),
-      finIcon: require("@/assets/img/sidebar/finIcon.svg"),
-      itIcon: require("@/assets/img/sidebar/itIcon.svg"),
-      offIcon: require("@/assets/img/sidebar/offIcon.svg"),
-      persIcon: require("@/assets/img/sidebar/persIcon.svg"),
-      desigIcon: require("@/assets/img/sidebar/desigIcon.svg"),
-      markIcon: require("@/assets/img/sidebar/markIcon.svg"),
-      lifeIcon: require("@/assets/img/sidebar/lifeIcon.svg"),
-      photoIcon: require("@/assets/img/sidebar/photoIcon.svg"),
-      healIcon: require("@/assets/img/sidebar/healIcon.svg"),
-      musIcon: require("@/assets/img/sidebar/musIcon.svg"),
-      teachIcon: require("@/assets/img/sidebar/teachIcon.svg"),
-    };
-  },
-};
+         const selectedCourse = computed(() => store.state.categories.category);
+
+         const test = async (i) => {
+            mainCategory.value = i;
+            context.emit("clickedCata", i, category.value, subCategory.value);
+            store.commit("SET_CATEGORY", {
+               main: mainCategory.value,
+            });
+            await store.dispatch("getAnyCollection", [mainCategory.value]);
+         };
+
+         const clickedCategories = ref(null);
+
+         const handleClick = (i) => {
+            if (clickedCategories.value !== i) {
+               clickedCategories.value = i;
+            } else {
+               clickedCategories.value = null;
+            }
+         };
+
+         return {
+            mainCategory,
+            category,
+            subCategory,
+            test,
+            cata,
+            handleClick,
+            clickedCategories,
+            arrowExpand: require("@/assets/img/sidebar/arrow.svg"),
+            devIcon: require("@/assets/img/sidebar/devIcon.svg"),
+            busIcon: require("@/assets/img/sidebar/busIcon.svg"),
+            finIcon: require("@/assets/img/sidebar/finIcon.svg"),
+            itIcon: require("@/assets/img/sidebar/itIcon.svg"),
+            offIcon: require("@/assets/img/sidebar/offIcon.svg"),
+            persIcon: require("@/assets/img/sidebar/persIcon.svg"),
+            desigIcon: require("@/assets/img/sidebar/desigIcon.svg"),
+            markIcon: require("@/assets/img/sidebar/markIcon.svg"),
+            lifeIcon: require("@/assets/img/sidebar/lifeIcon.svg"),
+            photoIcon: require("@/assets/img/sidebar/photoIcon.svg"),
+            healIcon: require("@/assets/img/sidebar/healIcon.svg"),
+            musIcon: require("@/assets/img/sidebar/musIcon.svg"),
+            teachIcon: require("@/assets/img/sidebar/teachIcon.svg"),
+         };
+      },
+   };
 </script>
