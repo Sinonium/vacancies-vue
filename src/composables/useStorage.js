52563bbb3414926a19acb6ea0fe57e87bdd7fa55
aidanadk <69148680+aidanadk@users.noreply.github.com>
2021-12-11 13:52:11 +0600

    added smth in useStorage.js

--- src/composables/useStorage.js
+++ src/composables/useStorage.js
@@ -4,7 +4,10 @@ import {storage} from '@/firebase/config'
 export const useStorage = () => {
     const uploadImageAndGetImageUrl = async (file) => {
        const imageRef = ref(storage, 'images/' + file.name)
-       const text = await uploadBytesResumable(imageRef,file)
+       const imageUrl = await uploadBytesResumable(imageRef,file)
+       const url = await getDownloadURL(imageUrl.ref)
+       console.log(url)
+
     };
 
     return {uploadImageAndGetImageUrl}
