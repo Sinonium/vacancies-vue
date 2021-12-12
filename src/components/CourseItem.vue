5cffc20cf0f206de51bb9595759c96f938e368d3
Sinonium <sinoniumx@gmail.com>
2021-12-12 18:29:00 +0600

    class router-link class

--- src/components/CourseItem.vue
+++ src/components/CourseItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="col-3">
     <div v-if="course" class="course-item">
-    <router-link :to="{name: 'DetailsCourse', params: {id: course.moreInfoId}}"  @click="handleMoreInfo()">
+    <router-link :to="{name: 'DetailsCourse', params: {id: course.moreInfoId}}"  @click="handleMoreInfo()" class="router-link">
       <div class="course-item__header">
         <img class="course-item__image" :src="course.imageUrl" alt="course" />
         <div class="course-item__time">
